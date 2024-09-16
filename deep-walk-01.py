6161ae4d6e681c85941e396344cdedc2e5d3c6d5
Gennady Khvorykh <info@inzilico.com>
2024-09-16 21:45:24 +0300

    scripts staged

---
+++ deep-walk-01.py
@@ -0,0 +1,83 @@
+"""
+Make embeddings of the graph with DeepWalk implemented in karateclub.
+The nodes are reindexed. 
+Author: Gennady Khvorykh
+Created: July 10, 2024
+"""
+
+import networkx as nx
+import sys, os
+from karateclub import DeepWalk
+import numpy as np
+import pandas as pd
+import helpers
+import time
+import argparse
+
+
+# Initiate argument parser
+parser = argparse.ArgumentParser(description="Make embeddings of the graph with DeepWalk \
+                                 implemented in karateclub.")
+parser.add_argument("-i", "--input", help="path/to/filename with agjacency list", required=True)
+parser.add_argument("-o", "--output", help="path/to/filename to save embeddings", required=True)
+parser.add_argument("-d", "--dimensions", help="The number of dimensions (default=128)", 
+                    default=128, type=int)
+parser.add_argument("-c", "--cpu", help="The number of CPU (default=10)", 
+                    default=10, type=int)
+parser.add_argument("-l", "--walk_length", help="The number of nodes in walk path (default=80)", 
+                    default=80, type=int)
+parser.add_argument("-n", "--walk_number", help="The number of random walks for each node (default=10)", 
+                    default=10, type=int)
+
+# Get command line arguments
+args = parser.parse_args()
+
+# Initiate variables
+input_file = args.input
+output_file = args.output
+cpu = args.cpu
+dimensions = args.dimensions
+walk_length = args.walk_length
+walk_number = args.walk_number
+ts = time.time()
+
+# Check input
+if not os.path.isfile(input_file):
+    print(input_file, "doesn't exist!")
+    sys.exit(1)
+
+# Show input
+print("Input:", input_file)
+print("Output:", output_file)
+print("Dimensions:", dimensions)
+print("CPU:", cpu)
+
+# Create graph from agjacency list 
+G1 = nx.read_adjlist(path=input_file)
+print("Nodes: ", len(G1.nodes))
+print("Edges: ", len(G1.edges))
+
+# Relabel nodes by consecutive integers
+G2, mapping = helpers.relabel_nodes(G1)
+
+# Crete estimator  
+model = DeepWalk(walk_number=walk_number, walk_length=walk_length, dimensions=dimensions, workers=cpu) 
+
+# Train model
+print("Training model...")
+model.fit(G2) 
+
+# Get embeddings
+embedding = model.get_embedding() 
+
+# Show embedding shape 
+print("Embedding shape:", embedding.shape)
+
+# Convert into data frame 
+df = pd.DataFrame(embedding, index=mapping.keys())
+
+# Save embeddings into file
+df.to_csv(output_file, sep=" ", header=False, index=True)
+
+# Show time elepsed
+helpers.show_time_elepsed(ts)
---
+++ helpers.py
@@ -0,0 +1,174 @@
+"""
+Custom functions to assisit data processing. 
+Author: Gennady Khvorykh 
+"""
+
+import networkx as nx
+import time
+import random
+import os, sys
+import pandas as pd
+from sklearn.preprocessing import minmax_scale
+from math import log10, ceil
+from sklearn.neighbors import NearestNeighbors
+import numpy as np
+import random
+
+from sklearn.model_selection import train_test_split, GridSearchCV 
+from sklearn.metrics import roc_auc_score 
+from sklearn.metrics import classification_report 
+from sklearn.metrics import RocCurveDisplay 
+from sklearn.svm import SVC
+
+
+def relabel_nodes(G: nx.graph) -> nx.graph:
+    """
+    Relabel nodes by consecutive integers if dictionary not provided or by 
+    values in dictionary.
+    """
+    # Get node labels
+    nodes = list(G.nodes)
+    # Initiate dictionary to keep old (keys) and new (values) node labels
+    mapping = {}
+    # Loop over all nodes
+    for i in range(len(nodes)):
+        # Fill dictionary 
+        mapping[nodes[i]] = i
+        
+    # Relabel nodes in place
+    nx.relabel_nodes(G=G, mapping=mapping, copy=False)
+    return(G, mapping)
+    
+def show_time_elepsed(ts):
+    # Show time elepsed
+    dur = time.time() - ts
+    s = time.strftime("%H:%M:%S", time.gmtime(dur))
+    print("\nTime elapsed:", s)
+    
+def get_random_walk(g: nx.graph, node: str, walk_length: int) -> list:
+    # Initiate variable
+    random_walk = [node]
+    # Iterate to build random walk of given path length
+    for _ in range(walk_length - 1):
+        neighbors = list(g.neighbors(node))
+        neighbors = list(set(neighbors) - set(random_walk))
+        if len(neighbors) == 0: break
+        random_node = random.choice(neighbors)
+        random_walk.append(random_node)
+        node = random_node
+    return random_walk
+
+def check_input(files: list):
+    for file in files:
+        if not os.path.isfile(file):
+            print(file, "doesn't exist")
+            sys.exit(1)
+
+def load_embeddings(x: str) -> tuple:
+    # Check input
+    check_input([x])
+
+    # Load input data
+    d1 = pd.read_csv(x, sep=" ", header=None)
+    print("Input:", x)
+    print("Shape:", d1.shape)
+
+    # Subset samples and data (embeddings)
+    samples = d1[0]
+    d2 = d1.iloc[:,1:]
+
+    # Scale data
+    d3 = minmax_scale(X=d2)
+    return samples, d3
+
+def load_data(x: str):
+    # Check input
+    check_input([x])
+    # Load data
+    data = pd.read_csv(x, sep=" ", header=None)
+
+    # Subset features, labels, and samples
+    X = data.iloc[:, 2:]
+    y = data[1]
+    samples = data[0]
+
+    return(X, y, samples)
+ 
+def epd(df: pd.DataFrame):
+     """
+     Run edge pattern detection 
+     """
+     # Convert input to Numpy array
+     x = df.to_numpy(copy=True)
+     print("Data:", x.shape)
+     
+     # Estimate k
+     N = x.shape[0]
+     k = ceil(5 * log10(N))
+     
+     # Find k nearest neighbors
+     knn = NearestNeighbors(n_neighbors=k)
+     knn.fit(x)
+     nn = knn.kneighbors(n_neighbors=k, return_distance=False)
+         
+     # Initiate variables
+     T = 0.1
+     x_edge = []
+     
+     for i, vec in enumerate(x):
+        # Calculate k-nn direction vector
+        v_i = []
+        for j in nn[i]:
+            v_ij = vec-x[j]
+            v_i.append(v_ij/np.linalg.norm(v_ij))
+        # Approximate normal vector
+        n_i = sum(v_i)
+        # Calculate theta
+        theta = [np.dot(np.transpose(v), n_i) for v in v_i] 
+        # Calculate l
+        l = 1/k*sum([t >= 0 for t in theta])
+        if l >= 1 - T:
+            x_edge.append(vec)
+        
+     return x_edge
+
+def partition (lst: list, n: int) -> list:
+    """
+    Shuffle and partition the list 
+    """
+    # Shuffle the list
+    random.shuffle(x=lst)
+    # Get devision
+    division = len(lst) / n
+    chunks =  [lst[round(division * i):round(division * (i + 1))] for i in range(n)]
+    return(chunks)
+
+def classify(X, y):
+    """
+    Apply binary clussificaton.
+    """
+    
+    # Initiate variables
+    cpu = 10
+    
+    # Split data into train and test ones
+    X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=42, stratify=y, test_size=0.33)
+    
+    # Defining parameter range 
+    parameters = {"C": [0.1, 1, 10, 100, 1000],
+                  "gamma": [1, 0.1, 0.01, 0.001, 0.0001]} 
+    # Initiate the model 
+    model = GridSearchCV(estimator=SVC(probability=True), param_grid=parameters, n_jobs=cpu) 
+  
+    # Fitting the model for grid search 
+    model.fit(X_train, y_train) 
+    
+    # Eveluate the model
+    y_pred = model.predict(X_test)
+    cr = classification_report(y_test, y_pred, zero_division=0, target_names=['class 0', 'class 1'])
+    score = model.score(X_test, y_test)
+    auc = roc_auc_score(y_test, y_pred)
+    
+    return(model, cr, score, auc)
+
+    
