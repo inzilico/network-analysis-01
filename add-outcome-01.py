c8b14fc8ae3894ed7921baa5fb8e011bc4fc3ce6
Gennady Khvorykh <info@inzilico.com>
2024-09-29 16:35:06 +0300

    main() was added

--- add-outcome-01.py
+++ add-outcome-01.py
@@ -5,33 +5,39 @@ Created: July 15, 2024
 """
 
 import pandas as pd
-import helpers
+from helpers import check_input, check_output_folder
 from collections import Counter
 import sys
 
-# Get command line arguments
-input_file1 = sys.argv[1] # path/to/embeddings.txt
-input_file2 = sys.argv[2] # path/to/file.txt with the list of genes with known association
-output_file = sys.argv[3] # path/to/file.txt to save output
+def main():
+    # Get command line arguments
+    input_file1 = sys.argv[1] # path/to/embeddings.txt
+    input_file2 = sys.argv[2] # path/to/file.txt with the list of nodes (genes) with known labels (association).
+    output_file = sys.argv[3] # path/to/file.txt to save output
 
-# Check input
-helpers.check_input([input_file1, input_file2])    
+    # Check input
+    check_input([input_file1, input_file2])
+    check_output_folder(output_file)
+        
+    # Load data
+    d1 = pd.read_csv(input_file1, header=None, sep=" ")
+    print(f'{input_file1}:', d1.shape)
+    d2 = pd.read_csv(input_file2, sep = "\t", usecols=["Gene", "Gene_id"])
+    print(f'{input_file2}:', d2.shape)
+    
+    # Left join
+    d3 = pd.merge(left=d1, right=d2, how="left", left_on=0, right_on="Gene_id", sort=False, indicator="Source")
 
-# Load data
-d1 = pd.read_csv(input_file1, header=None, sep=" ")
-print(f'{input_file1}:', d1.shape)
-d2 = pd.read_csv(input_file2, sep = "\t", usecols=["Gene", "Gene_id"])
-print(f'{input_file2}:', d2.shape)
+    # Create outcome variable
+    outcome = [1 if x == "both" else 0 for x in d3["Source"]]
+    print(Counter(outcome))
 
-# Left join
-d3 = pd.merge(left=d1, right=d2, how="left", left_on=0, right_on="Gene_id", sort=False, indicator="Source")
+    # Add column with outcome 
+    d1.insert(loc=1, column="outcome", value=outcome)
 
-# Create outcome variable
-outcome = [1 if x == "both" else 0 for x in d3["Source"]]
-print(Counter(outcome))
+    # Save into file
+    d1.to_csv(output_file, sep=" ", header=False, index=False)
 
-# Add column with coutcome 
-d1.insert(loc=1, column="outcome", value=outcome)
-
-# Save into file
-d1.to_csv(output_file, sep=" ", header=False, index=False)
+    
+if __name__ == "__main__":
+    main()
